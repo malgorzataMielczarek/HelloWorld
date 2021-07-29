cbc46abed557bb000ed8696b578ac3c082309107
Malgorzata Mielczarek <gosia1406@gmail.com>
2021-07-29 13:38:51 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Nazywam się Małgorzata Mielczarek.");
+        }
+    }
+}
